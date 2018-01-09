699a2a08f64ee1f73ab67903b868e71691ab0b4f
Unknown <andreas@stickydirt.se>
2018-01-09 14:21:35 +0100

    Created cards

--- www/js/main.js
+++ www/js/main.js
@@ -1,20 +1,37 @@
-$(document).on('click', 'a.pop', function(e) {
+class Deck {
+  constructor(){
+    this.deck = [];
+    this.createCards();
+  }
 
-let href = $(this).attr('href');
-history.pushState(null,null, href);
+   createCards() {
+     let cardSuits = ['Heart', 'Diamond', 'Spade', 'Club'];
 
-changePage();
+     for (let suit of cardSuits) {
+       for (let i = 1; i < 14; i++){
+         this.deck.push({
+           'CardColor' : suit,
+           'CardNumber' : i
+         });
+       }
+     }
 
-e.preventDefault();
 
 
-});
 
-function changePage() {
+  }
 
-let url = location.pathname;
-if(url == '/') {
-  
 }
 
-}
+let x = new Deck();
+
+
+// class Card {
+//   constructor(){
+//     this.hearts = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+//   }
+//   sendToDeck(item){
+//
+//
+//   }
+// }
